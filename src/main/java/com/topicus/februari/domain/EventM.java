9428899fbed1fc02a4d182f44a47ba5f5b1c4223
FelixvL <digitaleservice@gmail.com>
2023-02-14 11:23:21 +0100

    eventm

---
+++ src/main/java/com/topicus/februari/domain/EventM.java
@@ -0,0 +1,50 @@
+package com.topicus.februari.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class EventM {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String title;
+	private LocalDateTime starttime;
+	private LocalDateTime endtime;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public LocalDateTime getStarttime() {
+		return starttime;
+	}
+	public void setStarttime(LocalDateTime starttime) {
+		this.starttime = starttime;
+	}
+	public LocalDateTime getEndtime() {
+		return endtime;
+	}
+	public void setEndtime(LocalDateTime endtime) {
+		this.endtime = endtime;
+	}
+	
+	
+	
+	
+}
