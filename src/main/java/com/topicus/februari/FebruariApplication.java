02fe273bc3b556c75217f6f6ffd8047847229a94
Timo Bellert <t.bellert@workingspirit.nl>
2023-02-14 10:42:56 +0100

    first commit' '

---
+++ src/main/java/com/topicus/februari/FebruariApplication.java
@@ -0,0 +1,13 @@
+package com.topicus.februari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FebruariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FebruariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topicus/februari/FebruariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicus.februari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FebruariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
