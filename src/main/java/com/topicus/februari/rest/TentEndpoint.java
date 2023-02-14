c3053ae1f7180c39546553df1fcee57ab2993311
Timo Bellert <t.bellert@workingspirit.nl>
2023-02-14 14:07:00 +0100

    missing /

--- src/main/java/com/topicus/februari/rest/TentEndpoint.java
+++ src/main/java/com/topicus/februari/rest/TentEndpoint.java
@@ -11,32 +11,32 @@ import com.topicus.februari.domain.Tent;
 
 @RestController
 public class TentEndpoint {
-	@GetMapping("tent/ownextrainfo")
+	@GetMapping("/tent/ownextrainfo")
 	public void justTrying() {
 		System.out.println("Hallo");
 	}
 	
-	@PostMapping("tent/ownextrainfo")
+	@PostMapping("/tent/ownextrainfo")
 	public void justTrying2() {
 		System.out.println("Another");
 	}
 	
-	@GetMapping("tent/metpad/{eventname}")
+	@GetMapping("/tent/metpad/{eventname}")
 	public void metPad(@PathVariable("eventname") String eventnaam) {
 		System.out.println("met padnaam " + eventnaam);
 	}
 
-	@GetMapping("tent/metparam")
+	@GetMapping("/tent/metparam")
 	public void metParam(@RequestParam("getal") int getal, int nummer) {
 		System.out.println("param " + getal + nummer);
 	}
 	
-	@PostMapping("tent/metbody")
+	@PostMapping("/tent/metbody")
 	public void metBody(@RequestBody Tent e) {
 		System.out.println(e.getName());
 	}
 	
-	@GetMapping("tent/metevent")
+	@GetMapping("/tent/metevent")
 	public Tent metevent() {
 		System.out.println("Dit is een tent");
 		return new Tent();
